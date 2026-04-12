729e5dfb90c77f8f27b25b4ce6ac3999307fdd41
521w <54992297+521w@users.noreply.github.com>
2026-04-12 21:40:00 +0800

    更新 __init__.py

---  tabs/__init__.py
+++  tabs/__init__.py
@@ -5,6 +5,7 @@ from tabs.process import ProcessTab
 from tabs.terminal import TerminalTab
 from tabs.logviewer import LogViewerTab
 from tabs.logcat import LogcatTab
+from tabs.toolbox import ToolboxTab
 
 __all__ = [
     'SystemMonitorTab',
@@ -12,4 +13,5 @@ __all__ = [
     'TerminalTab',
     'LogViewerTab',
     'LogcatTab',
+    'ToolboxTab',
 ]
